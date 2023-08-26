7c42541e0a30bca6c8d6dc03173556bb874edd8d
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-08-26 14:57:37 -0400

    updated typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,49 +11,25 @@ const openai = new OpenAI({
 
 const resolvers = {
   Query: {
-    test: async () => {
-      return {
-        message: "It's working!"
-      }
-    },
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
-    },
-    //     chat: async (parent, { message }) => {
-    //       const chatCompletion = await openai.chat.completions.create({
-    //         messages: [
-    //           {"role": "system", "content": "I am your personal fitness, nutrition, and lifestyle coach. With your input, I will design workouts and meal plans based on your body type, lifestyle, and goals." },
-    //           {"role": "system", "content": "Before we begin, I need to ask you a few questions."},
-    //           {"role": "user", "content": `${message}`},
-    //         ],
-    //         //add macrobutton prompts here
+    // chat: async (parent, { message }) => {
+    //   const chatCompletion = await openai.chat.completions.create({
+    //     messages: [
+    //       { "role": "system", "content": "I am your personal fitness, nutrition, and lifestyle coach. With your input, I will design workouts and meal plans based on your body type, lifestyle, and goals." },
+    //       { "role": "system", "content": "Before we begin, I need to ask you a few questions." },
+    //       { "role": "user", "content": `${message}` },
+    //     ],
+    //     //add macrobutton prompts here
 
 
-    //         model: "gpt-3.5-turbo",      
-    //       });
-    //       console.log(JSON.stringify(chatCompletion, null, 2));
-    //       return {
-    //        message: JSON.stringify(chatCompletion.choices[0].message.content)
-    //       }
-    // },  
+    //     model: "gpt-3.5-turbo",
+    //   });
+    //   console.log(JSON.stringify(chatCompletion, null, 2));
+    //   return {
+    //     message: JSON.stringify(chatCompletion.choices[0].message.content)
+    //   }
+    // },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
-    },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
-    },
     chat2: async (parent, { message }) => {
       console.log(message);
       const chatCompletion = await openai.chat.completions.create({
@@ -68,7 +44,7 @@ const resolvers = {
 
       })
       // link button to backend
-      
+
       console.log(JSON.stringify(chatCompletion, null, 2));
       return {
         message: chatCompletion.choices[0].message.content
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,50 @@
 const typeDefs = `
+type User {
+  _id: ID!
+  username: String!
+  email: String!
+  profileData: [Profile]!
+}
+
+type Profile {
+  age: Int!
+  height: Int!
+  weight: Int!
+  gender: String!
+  activity: String!
+  goal: String!
+  diet: String!
+}
+
 type Message {
   message: String!
 }
 
-type Tech {
-    _id: ID!
-    name: String!
-  }
+type Auth {
+  token: ID!
+  user: User
+}
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
-  }
+input ProfileInput {
+  age: Int!
+  height: Int!
+  weight: Int!
+  gender: String!
+  activity: String!
+  goal: String!
+  diet: String!
+}
 
-  type Query {
-    test: Message
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
-    chat(message: String!): Message
-  }
+type Query {
+  me: User
+  chat(message: String!): Message
+}
 
-  type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
-    chat2(message: String!): Message
-  }
+type Mutation {
+  login(email: String!, password: String!): Auth
+  signUp(username: String!, email: String!, password: String!): Auth
+  chat2(input: ProfileInput!): Message
+}
 `;
 
 module.exports = typeDefs;
