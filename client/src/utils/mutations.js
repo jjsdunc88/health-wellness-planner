bbbfcb7eb6c6196280d66d092f657a41a708276f
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-08-26 16:44:11 -0400

    updated mutations.s on the client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,31 @@
 import { gql } from '@apollo/client';
 
+export const MUTATION_SIGNUP = gql`
+mutation SignUp($username: String!, $email: String!, $password: String!) {
+  signUp(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
+}
+`;
+
+export const MUTATION_LOGIN = gql`
+mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
+}
+`;
+
 export const MUTATION_MESSAGE = gql`
 mutation Chat2($message: String!) {
   chat2(message: $message) {
@@ -7,25 +33,3 @@ mutation Chat2($message: String!) {
   }
 }
 `;
-
-// export const CREATE_MATCHUP = gql`
-//   mutation createMatchup($tech1: String!, $tech2: String!) {
-//     createMatchup(tech1: $tech1, tech2: $tech2) {
-//       _id
-//       tech1
-//       tech2
-//     }
-//   }
-// `;
-
-// export const CREATE_VOTE = gql`
-//   mutation createVote($_id: String!, $techNum: Int!) {
-//     createVote(_id: $_id, techNum: $techNum) {
-//       _id
-//       tech1
-//       tech2
-//       tech1_votes
-//       tech2_votes
-//     }
-//   }
-// `;
