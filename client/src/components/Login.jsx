977cfa25c0ee48aebf4058164ea5108dd9443cf7
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-08-28 16:30:17 -0400

    worked on user signup and login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 import {
   LoginContainer,
   LoginForm,
@@ -8,21 +8,70 @@ import {
   SubmitButton,
 } from "../styled-components/Login-Style";
 
-function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+import { MUTATION_LOGIN } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+import Auth from '../utils/auth';
+
+export default function Login(props) {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [login, { error }] = useMutation(MUTATION_LOGIN);
+
+  const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    setShowError(false);
+    setShowSuccess(false);
+
+    try {
+      const { data } = await login({
+        variables: {
+          ...formState
+        }
+      });
+
+      Auth.login(data.login.token);
+      setShowSuccess(true);
+    } catch (err) {
+      console.error(err);
+      setShowError(true);
+    }
+
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
 
   return (
     <LoginContainer>
       <h2>Login</h2>
-      <LoginForm>
+      <LoginForm onSubmit={handleFormSubmit}>
         <FormInputGroup>
-          <FormLabel htmlFor="username">Username:</FormLabel>
+          <FormLabel htmlFor="email">Email:</FormLabel>
           <FormInput
-            type="text"
-            id="username"
-            name="username"
-            onChange={(e) => setUsername(e.target.value)}
+            type="email"
+            id="email"
+            name="email"
+            value={formState.email}
+            onChange={handleChange}
           />
         </FormInputGroup>
         <FormInputGroup>
@@ -31,13 +80,58 @@ function Login() {
             type="password"
             id="password"
             name="password"
-            onChange={(e) => setPassword(e.target.value)}
+            value={formState.password}
+            onChange={handleChange}
           />
         </FormInputGroup>
         <SubmitButton type="submit" value="Submit" className="submitBtn" />
+        {showError ? (
+          <h4 style={{ color: "red" }}>
+            Wrong password!
+          </h4>
+        ) : (
+          <></>
+        )}
+        {showSuccess ? (
+          <h4 style={{ color: "green" }}>
+            Good Login!
+          </h4>
+        ) : (
+          <></>
+        )}
       </LoginForm>
     </LoginContainer>
-  );
+  )
 }
 
-export default Login;
+// function Login() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   return (
+//     <LoginContainer>
+//       <h2>Login</h2>
+//       <LoginForm>
+//         <FormInputGroup>
+//           <FormLabel htmlFor="username">Username:</FormLabel>
+//           <FormInput
+//             type="text"
+//             id="username"
+//             name="username"
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+//         </FormInputGroup>
+//         <FormInputGroup>
+//           <FormLabel htmlFor="password">Password:</FormLabel>
+//           <FormInput
+//             type="password"
+//             id="password"
+//             name="password"
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </FormInputGroup>
+//         <SubmitButton type="submit" value="Submit" className="submitBtn" />
+//       </LoginForm>
+//     </LoginContainer>
+//   );
+// }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   SignUpContainer,
   SignUpForm,
@@ -8,31 +8,84 @@ import {
   SubmitButton,
 } from "../styled-components/SignUpPage-Style";
 
-function SignUp() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+import { useMutation } from "@apollo/client";
+import { MUTATION_SIGNUP } from "../utils/mutations";
+
+import Auth from '../utils/auth';
+
+export default function SignUp() {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [signUp, { error }] = useMutation(MUTATION_SIGNUP);
+
+  const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    setShowError(false);
+    setShowSuccess(false);
+
+    try {
+      const { data } = await signUp({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.signUp.token);
+      setShowSuccess(true);
+    } catch (err) {
+      console.error(err);
+      setShowError(true);
+    }
+  };
 
   return (
     <SignUpContainer>
       <h2>Sign Up</h2>
-      <SignUpForm>
+      <SignUpForm onSubmit={handleFormSubmit}>
         <FormInputGroup>
           <FormLabel htmlFor='username'>Username: </FormLabel>
-          <FormInput type='text' id='username' name='username' onChange={(e) => setUsername(e.target.value)}/>
-        </FormInputGroup>
-        <FormInputGroup>
-          <FormLabel htmlFor='password'>Password: </FormLabel>
-          <FormInput type='password' id='password' name='password' onChange={(e) => setPassword(e.target.value)}/>
+          <FormInput type='text' id='username' name='username' onChange={handleChange} />
         </FormInputGroup>
         <FormInputGroup>
           <FormLabel htmlFor='email'>Email: </FormLabel>
-          <FormInput type='email' id='email' name='email' onChange={(e) => setEmail(e.target.value)}/>
+          <FormInput type='email' id='email' name='email' onChange={handleChange} />
+        </FormInputGroup>
+        <FormInputGroup>
+          <FormLabel htmlFor='password'>Password: </FormLabel>
+          <FormInput type='password' id='password' name='password' onChange={handleChange} />
         </FormInputGroup>
         <SubmitButton type='submit' value='Submit' />
+        {showError ? (
+          <h4 style={{ color: "red" }}>
+            Wrong password!
+          </h4>
+        ) : (
+          <></>
+        )}
+        {showSuccess ? (
+          <h4 style={{ color: "green" }}>
+            Good Login!
+          </h4>
+        ) : (
+          <></>
+        )}
       </SignUpForm>
     </SignUpContainer>
   );
 }
-
-export default SignUp;
