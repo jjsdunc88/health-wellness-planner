48557519264035d31c944d4debb855db7bd502de
JDunc <jjsduncan@gmail.com>
2023-10-27 17:39:24 -0400

    remove commented out code from Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -114,34 +114,3 @@ export default function Login(props) {
   )
 }
 
-// function Login() {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   return (
-//     <LoginContainer>
-//       <h2>Login</h2>
-//       <LoginForm>
-//         <FormInputGroup>
-//           <FormLabel htmlFor="username">Username:</FormLabel>
-//           <FormInput
-//             type="text"
-//             id="username"
-//             name="username"
-//             onChange={(e) => setUsername(e.target.value)}
-//           />
-//         </FormInputGroup>
-//         <FormInputGroup>
-//           <FormLabel htmlFor="password">Password:</FormLabel>
-//           <FormInput
-//             type="password"
-//             id="password"
-//             name="password"
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//         </FormInputGroup>
-//         <SubmitButton type="submit" value="Submit" className="submitBtn" />
-//       </LoginForm>
-//     </LoginContainer>
-//   );
-// }
